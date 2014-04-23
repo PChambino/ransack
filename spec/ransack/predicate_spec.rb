d52d324d3bd1c6cf6b1c5a1f6b866dbd1650222d
Pedro Chambino <pchambino@gmail.com>
2014-04-23 23:42:16 +0100

    Add spec to support false values on predicates

--- spec/ransack/predicate_spec.rb
+++ spec/ransack/predicate_spec.rb
@@ -41,8 +41,167 @@ module Ransack
       end
     end
 
+    describe 'present' do
+      it 'generates a condition for boolean true' do
+        @s.name_present = true
+        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
+        @s.result.to_sql.should match /#{field} IS NOT NULL AND #{field} != ''/
+      end
+
+      it 'generates a condition for boolean false' do
+        @s.name_present = false
+        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
+        @s.result.to_sql.should match /NOT \(\(#{field} IS NOT NULL AND #{field} != ''\)\)/
+      end
+
+      it 'does not generate a condition for nil' do
+        @s.name_present = nil
+        @s.result.to_sql.should_not match /WHERE/
+      end
+    end
+
+    describe 'blank' do
+      it 'generates a condition for boolean true' do
+        @s.name_blank = true
+        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
+        @s.result.to_sql.should match /#{field} IS NULL OR #{field} = ''/
+      end
+
+      it 'generates a condition for boolean false' do
+        @s.name_blank = false
+        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
+        @s.result.to_sql.should match /NOT \(\(#{field} IS NULL OR #{field} = ''\)\)/
+      end
+
+      it 'does not generate a condition for nil' do
+        @s.name_blank = nil
+        @s.result.to_sql.should_not match /WHERE/
+      end
+    end
+
+    describe 'true' do
+      it 'generates a condition for boolean true' do
+        @s.awesome_true = true
+        field = "#{quote_table_name("people")}.#{quote_column_name("awesome")}"
+        @s.result.to_sql.should match /#{field} = #{
+          ActiveRecord::Base.connection.quoted_true}/
+      end
+
+      it 'generates a condition for boolean false' do
+        @s.awesome_true = false
+        field = "#{quote_table_name("people")}.#{quote_column_name("awesome")}"
+        @s.result.to_sql.should match /NOT \(#{field} = #{
+          ActiveRecord::Base.connection.quoted_true}\)/
+      end
+
+      it 'does not generate a condition for nil' do
+        @s.awesome_true = nil
+        @s.result.to_sql.should_not match /WHERE/
+      end
+    end
+
+    describe 'not_true' do
+      it 'generates a condition for boolean true' do
+        @s.awesome_not_true = true
+        field = "#{quote_table_name("people")}.#{quote_column_name("awesome")}"
+        @s.result.to_sql.should match /#{field} != #{
+          ActiveRecord::Base.connection.quoted_true}/
+      end
+
+      it 'generates a condition for boolean false' do
+        @s.awesome_not_true = false
+        field = "#{quote_table_name("people")}.#{quote_column_name("awesome")}"
+        @s.result.to_sql.should match /NOT \(#{field} != #{
+          ActiveRecord::Base.connection.quoted_true}\)/
+      end
+
+      it 'does not generate a condition for nil' do
+        @s.awesome_not_true = nil
+        @s.result.to_sql.should_not match /WHERE/
+      end
+    end
+
+    describe 'false' do
+      it 'generates a condition for boolean true' do
+        @s.awesome_false = true
+        field = "#{quote_table_name("people")}.#{quote_column_name("awesome")}"
+        @s.result.to_sql.should match /#{field} = #{
+          ActiveRecord::Base.connection.quoted_false}/
+      end
+
+      it 'generates a condition for boolean false' do
+        @s.awesome_false = false
+        field = "#{quote_table_name("people")}.#{quote_column_name("awesome")}"
+        @s.result.to_sql.should match /NOT \(#{field} = #{
+          ActiveRecord::Base.connection.quoted_false}\)/
+      end
+
+      it 'does not generate a condition for nil' do
+        @s.awesome_false = nil
+        @s.result.to_sql.should_not match /WHERE/
+      end
+    end
+
+    describe 'not_false' do
+      it 'generates a condition for boolean true' do
+        @s.awesome_not_false = true
+        field = "#{quote_table_name("people")}.#{quote_column_name("awesome")}"
+        @s.result.to_sql.should match /#{field} != #{
+          ActiveRecord::Base.connection.quoted_false}/
+      end
+
+      it 'generates a condition for boolean false' do
+        @s.awesome_not_false = false
+        field = "#{quote_table_name("people")}.#{quote_column_name("awesome")}"
+        @s.result.to_sql.should match /NOT \(#{field} != #{
+          ActiveRecord::Base.connection.quoted_false}\)/
+      end
+
+      it 'does not generate a condition for nil' do
+        @s.awesome_not_false = nil
+        @s.result.to_sql.should_not match /WHERE/
+      end
+    end
+
+    describe 'null' do
+      it 'generates a condition for boolean true' do
+        @s.name_null = true
+        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
+        @s.result.to_sql.should match /#{field} IS NULL/
+      end
+
+      it 'generates a condition for boolean false' do
+        @s.name_null = false
+        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
+        @s.result.to_sql.should match /NOT \(#{field} IS NULL\)/
+      end
+
+      it 'does not generate a condition for nil' do
+        @s.name_null = nil
+        @s.result.to_sql.should_not match /WHERE/
+      end
+    end
+
+    describe 'not_null' do
+      it 'generates a condition for boolean true' do
+        @s.name_not_null = true
+        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
+        @s.result.to_sql.should match /#{field} IS NOT NULL/
+      end
+
+      it 'generates a condition for boolean false' do
+        @s.name_not_null = false
+        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
+        @s.result.to_sql.should match /NOT \(#{field} IS NOT NULL\)/
+      end
+
+      it 'does not generate a condition for nil' do
+        @s.name_not_null = nil
+        @s.result.to_sql.should_not match /WHERE/
+      end
+    end
+
     describe 'cont' do
-  
       it_has_behavior 'wildcard escaping', :name_cont,
         (if ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
           /"people"."name" ILIKE '%\\%\\._\\\\%'/
@@ -77,22 +236,6 @@ module Ransack
         @s.name_not_cont = 'ric'
         field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
         @s.result.to_sql.should match /#{field} NOT I?LIKE '%ric%'/
-      end
-    end
-
-    describe 'null' do
-      it 'generates a value IS NULL query' do
-        @s.name_null = true
-        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
-        @s.result.to_sql.should match /#{field} IS NULL/
-      end
-    end
-
-    describe 'not_null' do
-      it 'generates a value IS NOT NULL query' do
-        @s.name_not_null = true
-        field = "#{quote_table_name("people")}.#{quote_column_name("name")}"
-        @s.result.to_sql.should match /#{field} IS NOT NULL/
       end
     end
   end
